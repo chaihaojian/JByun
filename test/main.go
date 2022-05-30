3230c5ba373ebea1f8772c8abdbfb215aaf18a81
柴皓舰 <admin@chaihaojiandeMacBook-Air.local>
2022-05-30 15:34:49 +0800

    实现分块上传初始化

---
+++ test/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+)
+
+// 声明一个全局的rdb变量
+var rdb *redis.Client
+
+// 初始化连接
+func initClient() (err error) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	_, err = rdb.Ping().Result()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	if err := initClient(); err != nil {
+		fmt.Println("init failed")
+	}
+	fmt.Printf("success")
+	defer rdb.Close()
+}
